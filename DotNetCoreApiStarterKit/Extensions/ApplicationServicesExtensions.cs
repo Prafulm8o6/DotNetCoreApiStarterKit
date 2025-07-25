e27b4a980ef7878876ac68060a56cdb12c447276
Praful Mahajan <prafulmahajan806@gmail.com>
2025-07-25 21:42:39 +0530

    feat: Implement ApplicationServicesExtensions and integrate into Program.cs

---
+++ DotNetCoreApiStarterKit/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetCoreApiStarterKit.API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- DotNetCoreApiStarterKit/Program.cs
+++ DotNetCoreApiStarterKit/Program.cs
@@ -1,3 +1,5 @@
+using DotNetCoreApiStarterKit.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
